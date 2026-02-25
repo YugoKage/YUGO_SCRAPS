e3da6160afa88fafdbbb13340be39d6c8ca22870
Matt Lyndon Abuel <mattlyndonabuel@Matts-MacBook-Air.local>
2026-02-25 11:56:10 +0800

    InsertionSort Change

--- PY/Scrap19_InsertionSort.py
+++ PY/Scrap19_InsertionSort.py
@@ -1,21 +1,28 @@
 
 
-def InsSort(Array):
+def InSort(Array):
 
-    Leng = len(Array)
+    for main in range(0, len(Array) - 1):
+        minIndex = main
 
-    
+        for switch in range(main + 1, len(Array)):
+            if Array[switch] < Array[minIndex]:
+                minIndex = switch
+        
+        if minIndex != main:
+            Array[main], Array[minIndex] = Array[minIndex], Array[main]
 
-
-
+    return Array
 
 
 
 Input = [5, 3, 7, 2, 9, 2, 1]
 
+print("List:", Input)
 
+Input = InSort(Input)
 
-
+print("Sorted:", Input)
 
 
 
